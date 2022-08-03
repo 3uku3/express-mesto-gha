16bb6349f9239db6b9fc25915f7ea6f82e003f42
Mikhail Utin <utinspb3@yandex.ru>
2022-08-03 10:34:34 +0300

    fix: add validation in signup

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.post('/users/signup', celebrate({
   body: {
     email: Joi.string().required().email(),
     password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().required().regex(/^http(s)?:\/\/([\w.]+\/?)\S*/),
   },
 }), createUser);
 app.post('/users/signin', login);
